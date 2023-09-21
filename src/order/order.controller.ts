fdd9cea11c53fdd81118b11453fdba60e5999ea1
hellprise <mleokha@gmail.com>
2023-09-21 14:12:10 +0300

    payment (not finished)

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get } from '@nestjs/common'
+import {
+	Body,
+	Controller,
+	Get,
+	HttpCode,
+	Post,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common'
 import { Auth } from 'src/auth/decorators/auth.decorator'
 import { CurrentUser } from 'src/auth/decorators/user.decorator'
+import { OrderDto } from './dto/order.dto'
+import { PaymentStatusDto } from './dto/payment-status.dto'
 import { OrderService } from './order.service'
 
 @Controller('orders')
@@ -11,5 +21,19 @@ export class OrderController {
 	@Auth()
 	getAll(@CurrentUser('id') id: number) {
 		return this.orderService.getAll(id)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post()
+	@Auth()
+	placeOrder(@Body() dto: OrderDto, @CurrentUser('id') id: number) {
+		return this.orderService.placeOrder(dto, id)
+	}
+
+	@HttpCode(200)
+	@Post('status')
+	async updateStatus(@Body() dto: PaymentStatusDto) {
+		return this.orderService.updateStatus(dto)
 	}
 }
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,5 +1,8 @@
 import { Injectable, NotFoundException } from '@nestjs/common'
 import { PrismaService } from 'src/prisma.service'
+import { returnProductObject } from 'src/product/return-product.object'
+import { OrderDto } from './dto/order.dto'
+import { PaymentStatusDto } from './dto/payment-status.dto'
 
 @Injectable()
 export class OrderService {
@@ -14,11 +17,72 @@ export class OrderService {
 			},
 			orderBy: {
 				createdAt: 'desc'
+			},
+			include: {
+				items: {
+					include: {
+						product: {
+							select: returnProductObject
+						}
+					}
+				}
 			}
 		})
 
 		if (!orders) throw new NotFoundException('Orders not found')
 
 		return orders
+	}
+
+	async placeOrder(dto: OrderDto, userId: number) {
+		const total = dto.items.reduce((acc, item) => {
+			return acc + item.quantity * item.price
+		}, 0)
+
+		const order = await this.prisma.order.create({
+			data: {
+				status: dto.status,
+				items: {
+					create: dto.items
+				},
+				total,
+				user: {
+					connect: {
+						id: userId
+					}
+				}
+			}
+		})
+
+		// here will be the logic for payment
+
+		return order
+	}
+
+	async updateStatus(dto: PaymentStatusDto) {
+		if (dto.event === 'payment.waiting_for_capture') {
+			// const payment = await ...
+			const payment = 'payment is not implemented yet'
+
+			return payment
+		}
+
+		if (dto.event === 'payment.succeeded') {
+			const orderId = Number(dto.object.description.split('#')[1])
+
+			await this.prisma.order.update({
+				where: {
+					// id: +dto.object.id
+					id: orderId
+				},
+				data: {
+					status: 'PAYED'
+				}
+			})
+
+			return true
+		}
+
+		return 'Success'
 	}
 }
